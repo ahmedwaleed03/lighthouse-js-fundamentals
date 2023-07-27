6df319679a2139c1069ff1a701cf2c21f978b844
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-07-27 05:02:24 -0400

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -6,6 +6,10 @@ if (raining) {
   console.log("Don't forget your umbrella!");
 }
 
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
 if (cold) {
   console.log("Make sure you pick out a scarf!");
 }
@@ -23,14 +27,26 @@ if (cold) {
 }
 */
 
-const temperature = 12;
+/*
+
+const temperature = 24;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
   console.log("Short sleeves won't cut it!");
+}  else if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
 } else {
   console.log("Short sleeves are fine.");
 }
 
 console.log("Now you're ready to go outside!");
+*/
+
+const isCitizen = true;
+const age = 35;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
