1b25c93dabdb8c3a5a037ce5aa22703008067b98
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-07-28 15:37:01 -0400

    lastIndexOf activity

---
+++ lastIndexOf.js
@@ -0,0 +1,12 @@
+// function
+function lastIndexOf(arr, value) {
+  let index = arr.lastIndexOf(value);
+  return index;
+}
+
+// test the function
+console.log(lastIndexOf([0, 1, 4, 1, 2], 1), "=?", 3);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 2), "=?", 4);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 3), "=?", -1);
+console.log(lastIndexOf([5, 5, 5], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
