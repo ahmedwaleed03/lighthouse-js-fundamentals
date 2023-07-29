a0396d26a191c7088fdef23cbabde00c24ceb8cf
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-07-29 04:08:19 -0400

    concat activity

---
+++ concatenate.js
@@ -0,0 +1,11 @@
+// function
+function concat(arr1, arr2) {
+  var finalArray = arr1.concat(arr2);
+  return finalArray;
+}
+
+// test the function
+console.log(concat([1, 2, 3], [4, 5, 6]), "=?", [1, 2, 3, 4, 5, 6]);
+console.log(concat([0, 3, 1], [9, 7, 2]), "=?", [0, 3, 1, 9, 7, 2]);
+console.log(concat([], [9, 7, 2]), "=?", [9, 7, 2]);
+console.log(concat([5, 10], []), "=?", [5, 10]);
